edee99c392001d3ee06257ba1265f0c9759e5e25
Herbert Lange <lange@ids-mannheim.de>
2023-09-04 15:22:39 +0200

    use maven wrapper in install-pom.sh

--- install-pom.sh
+++ install-pom.sh
@@ -1,2 +1,2 @@
-mvn clean compile package install
-mvn install:install-file -Dfile=target/datacite-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=datacite-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
+./mvnw clean compile package install
+./mvnw install:install-file -Dfile=target/datacite-java-api-0.0.1-SNAPSHOT.jar -DgroupId=de.ids-mannheim.lza -DartifactId=datacite-java-api -Dversion=1.0 -Dpackaging=jar  -DgeneratePom=true
