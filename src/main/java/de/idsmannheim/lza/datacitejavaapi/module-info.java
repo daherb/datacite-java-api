6b292d47078cdc2c4e3eb496f4ad53486baebad4
Herbert Lange <github@pietaetskirsche.de>
2023-12-29 12:48:12 +0100

    add (implict) dependencies

--- src/main/java/de/idsmannheim/lza/datacitejavaapi/module-info.java
+++ src/main/java/de/idsmannheim/lza/datacitejavaapi/module-info.java
@@ -2,7 +2,17 @@ module datacite.java.api {
     requires java.logging;
 
     requires transitive java.sql;
+    requires gson.fire;
     requires com.google.gson;
+    requires okhttp3;
+    requires okhttp3.logging;
+    requires okio;
+    requires java.ws.rs;
+    requires java.activation;
+    requires java.annotation;
+    requires com.fasterxml.jackson.databind;
+    requires org.openapitools.jackson.nullable;
+    requires invenio.java.api;
 
     exports de.idsmannheim.lza.datacitejavaapi;
     exports org.datacite;
